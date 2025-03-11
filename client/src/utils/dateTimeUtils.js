9ef4be97d355745d387bb174b0d575cb791c9c17
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2025-03-11 11:50:23 +0200

    Format iso date

---
+++ client/src/utils/dateTimeUtils.js
@@ -0,0 +1,6 @@
+export const fromIsoDate = (isoDate) => {
+    const date = new Date(isoDate);
+    const formatedDate = date.toLocaleString('en-Us', {year: 'numeric', month: 'long', day: 'numeric'});
+
+    return formatedDate;
+}
